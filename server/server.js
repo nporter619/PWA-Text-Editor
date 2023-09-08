535c7f3128b26112dd62014dc4fd3304c5674950
nporter <nporterwork@gmail.com>
2023-09-07 21:35:17 -0700

    added path.join to server js hoping this helps

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
